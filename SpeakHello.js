612d3c46672538bf2c9e6c597661770a4c3de1cd
vbskrishna <128309206+vbskrishna@users.noreply.github.com>
2023-04-30 00:43:03 +0530

    Added files

---
+++ SpeakHello.js
@@ -0,0 +1,13 @@
+(function (window) {
+  var helloSpeaker = {
+  };
+
+  helloSpeaker.speak = function(name){
+    console.log(speakWord + " " + name);
+  }
+
+  var speakWord = "Hello";
+
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
