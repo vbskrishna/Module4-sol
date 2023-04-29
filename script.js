9e22739ff739506d62bc7e0c8ebef4a81556b8eb
vbskrishna <128309206+vbskrishna@users.noreply.github.com>
2023-04-30 01:01:41 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+// (function () {
+//   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+//   for (var i = 0; i < names.length; i++) {
+//     var firstLetter = names[i].charAt(0).toLowerCase();
+
+//     if (firstLetter === 'j') {
+//       byeSpeaker.speak(names[i]);
+//     } else {
+//       helloSpeaker.speak(names[i]);
+//     }
+//   }
+// })();
+
 (function () {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-  for (var i = 0; i < names.length; i++) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-    if (firstLetter === 'j') {
-      byeSpeaker.speak(names[i]);
-    } else {
-      helloSpeaker.speak(names[i]);
-    }
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
   }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
 })();
